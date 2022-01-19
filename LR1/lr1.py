f21f21726afda0f6bf7290fa0f431c1e11aa0d93
Sergey <icicestparis06@gmail.com>
2022-01-19 08:19:58 +0300

    lr

---
+++ LR1/lr1.py
@@ -0,0 +1,24 @@
+# void lev() {
+#     float h, s, x, res;
+#     h = (b - a) / n;
+#     s = 0;
+#     x = a;
+#     while (x <= (b - h))
+#     {
+#       s += fun(x);
+#       x += h;
+#     }
+#     res = h * s;
+#     printf("\n > Результат методом левых частей:  %.4f\n", res);
+#      float r = 0, m = 0;
+#  if (pro(a) > pro(b)) {
+#    m = fabs(pro(a));
+#  }
+#  else{
+#    m = fabs(pro(b));
+#  }
+#  r = (pow(b - a, 2)/(2*n))*m;
+#  printf(" > Остаточный член равен: %f\n", r);
+#    secondMenu();
+# }
+
